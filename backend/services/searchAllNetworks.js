733814db5a3be2800a2bad04be29be9129a246a7
Maryam Khan <maryam.khan@pcc.edu>
2023-02-17 11:54:03 -0800

    Added tiktok username checking functionality

--- backend/services/searchAllNetworks.js
+++ backend/services/searchAllNetworks.js
@@ -1,14 +1,17 @@
 const { searchTwitter } = require("./searchTwitter");
 const { searchTwitch } = require("./searchTwitch");
 const { searchReddit } = require("./searchReddit");
+const { searchTiktok } = require("./searchTiktok");
 const searchAllNetworks = async (username) => {
   const twitterData = await searchTwitter(username);
   const twitchData = await searchTwitch(username);
   const redditData = await searchReddit(username);
+  const tikTokData = await searchTiktok(username);
   return {
     twitterData: twitterData,
     twitchData: twitchData,
     redditData: redditData,
+    tikTokData: tikTokData,
   };
 };
 module.exports = { searchAllNetworks };
---
+++ backend/services/searchTiktok.js
@@ -0,0 +1,64 @@
+const puppeteer = require("puppeteer");
+const BASE_URL = "https://www.tiktok.com/";
+const scrapeTiktok = async (searchURL) => {
+  let userAvailable = false;
+  let errors = null;
+  const browser = await puppeteer.launch({
+    headless: true,
+    userDataDir: "./data",
+    fastStart: true,
+  });
+  const page = await browser.pages().then((e) => e[0]);
+  await page.goto(searchURL);
+
+  try {
+    let noSuchUser = await page.$x(
+      '//div[@class="tiktok-1osbocj-DivErrorContainer emuynwa0"]'
+    );
+    if (noSuchUser.length > 0) {
+      userAvailable = true;
+    } else {
+      userAvailable = false;
+    }
+  } catch (error) {
+    errors = { error };
+  }
+
+  await page.close();
+  await browser.close();
+
+  return { errors, userAvailable };
+};
+
+const generateErrorMessages = (scrapeError, username) => {
+  return scrapeError
+    ? {
+        // Error in scraping Tiktok
+        msg: `Unknown error occurred when scraping Tiktok for the username [${username}]. Please try again at another time.`,
+      }
+    : {
+        // Twitch username already exists
+        msg: `Error: The username [${username}] is taken.`,
+      };
+};
+
+const obtainTiktokProfile = async (username) => {
+  let { errors, userAvailable } = await scrapeTiktok(`${BASE_URL}@${username}`);
+  if (errors) {
+    console.log(errors);
+    // true if error has to do with tiktok scraping
+    return { ...generateErrorMessages(true, username), status: 400 };
+  } else if (!userAvailable) {
+    return { ...generateErrorMessages(false, username), status: 400 };
+  } else {
+    return {
+      msg: `The username ${username} is available!`,
+      status: 200,
+    };
+  }
+};
+const searchTiktok = async (username) => {
+  return await obtainTiktokProfile(username);
+};
+
+module.exports = { searchTiktok };
