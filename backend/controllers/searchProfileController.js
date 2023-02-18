9872781a87ff1f2277a72a844b4c229773c8bc87
Maryam Khan <maryam.khan@pcc.edu>
2023-02-17 21:06:00 -0800

    Trying promise instead of async/await in searchProfileController

--- backend/controllers/searchProfileController.js
+++ backend/controllers/searchProfileController.js
@@ -1,7 +1,7 @@
 const { searchAllNetworks } = require("../services/searchAllNetworks");
-const searchProfilesController = async (req, res) => {
-  const results = await searchAllNetworks(req.params.username);
-  console.log(results);
-  res.status(200).json(results);
+const searchProfilesController = (req, res) => {
+  searchAllNetworks(req.params.username).then((results) => {
+    res.status(200).json(results);
+  });
 };
 module.exports = { searchProfilesController };
