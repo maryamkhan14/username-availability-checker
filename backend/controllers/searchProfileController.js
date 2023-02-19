d146e1a869eb501d0b0943c06623814c8f203f85
Maryam Khan <maryam.khan@pcc.edu>
2023-02-18 16:28:56 -0800

    Fixed reference error with searchProfileController

--- backend/controllers/searchProfileController.js
+++ backend/controllers/searchProfileController.js
@@ -1,7 +1,7 @@
-const { searchTwitter } = require("./searchTwitter");
-const { searchTwitch } = require("./searchTwitch");
-const { searchReddit } = require("./searchReddit");
-const { searchTiktok } = require("./searchTiktok");
+const { searchTwitter } = require("../services/searchTwitter");
+const { searchTwitch } = require("../services/searchTwitch");
+const { searchReddit } = require("../services/searchReddit");
+const { searchTiktok } = require("../services/searchTiktok");
 const { searchAllNetworks } = require("../services/searchAllNetworks");
 const searchProfilesController = (req, res) => {
   res.setHeader(200, { "Content-Type": "application/json" });
