75a4c3796adc5b4313b9836e5c8de8b036f1c554
Maryam Khan <maryam.khan@pcc.edu>
2023-02-18 16:22:40 -0800

    Experimenting with res.write() instead of res.send() to see effects on results return rate

--- backend/controllers/searchProfileController.js
+++ backend/controllers/searchProfileController.js
@@ -1,6 +1,23 @@
+const { searchTwitter } = require("./searchTwitter");
+const { searchTwitch } = require("./searchTwitch");
+const { searchReddit } = require("./searchReddit");
+const { searchTiktok } = require("./searchTiktok");
 const { searchAllNetworks } = require("../services/searchAllNetworks");
-const searchProfilesController = async (req, res) => {
-  let results = await searchAllNetworks(req.params.username);
-  res.status(200).json(results);
+const searchProfilesController = (req, res) => {
+  res.setHeader(200, { "Content-Type": "application/json" });
+
+  searchTwitter(req.params.username).then((twitterData) =>
+    res.write({ twitterData: twitterData })
+  );
+  searchTwitch(req.params.username).then((twitchData) => {
+    res.write({ twitchData: twitchData });
+  });
+  searchReddit(req.params.username).then((redditData) => {
+    res.write({ redditData: redditData });
+  });
+  searchTiktok(req.params.username).then((tiktokData) => {
+    res.write({ tiktokData: tiktokData });
+  });
+  res.end();
 };
 module.exports = { searchProfilesController };
