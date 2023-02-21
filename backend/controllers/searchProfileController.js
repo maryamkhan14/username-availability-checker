54d571a5d52f240e8c3feed8ef98314a34f1a812
Maryam Khan <maryam.khan@pcc.edu>
2023-02-20 19:10:04 -0800

    Added res.end() line in searchProfileController

--- backend/controllers/searchProfileController.js
+++ backend/controllers/searchProfileController.js
@@ -36,6 +36,7 @@ const searchProfilesController = async (req, res) => {
   await tiktokSearch(req.params.username).then((result) => {
     res.write(JSON.stringify(result));
     res.write("\n\n");
+    res.end();
   });
 };
 module.exports = { searchProfilesController };
