8babe7d230e5c51398504e179e1d140af0bada53
Maryam Khan <maryam.khan@pcc.edu>
2023-02-20 19:42:20 -0800

    Added extra cors line

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const searchProfileRoutes = require("./routes/searchProfiles");
 const app = express();
 
 app.use(cors());
+app.options("*", cors());
 app.use(express.json());
 
 app.use("/search", searchProfileRoutes);
