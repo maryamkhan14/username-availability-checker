6ed85b5eeb4b62cb41957adcc591e56e4524e9a5
Maryam Khan <maryam.khan@pcc.edu>
2023-02-20 20:07:51 -0800

    Exposed other CORS headers

--- backend/controllers/searchProfileController.js
+++ backend/controllers/searchProfileController.js
@@ -17,7 +17,8 @@ const tiktokSearch = (username) => {
 };
 const searchProfilesController = async (req, res) => {
   res.writeHead(200, "Access-Control-Allow-Origin: *", {
-    "Access-Control-Expose-Headers": "Access-Control-Allow-Origin",
+    "Access-Control-Expose-Headers":
+      "*, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Access-Control-Allow-Methods, Access-Control-Max-Age",
     "Content-Type": "text/event-stream",
     Connection: "keep-alive",
     "Cache-Control": "no-cache",
