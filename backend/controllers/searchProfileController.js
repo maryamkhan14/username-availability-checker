b5bc4288c37a49d3f50fbe35dfa5dc46fd29dee6
Maryam Khan <maryam.khan@pcc.edu>
2023-02-20 20:43:43 -0800

    Cleaned up promise code for returning responses in searchProfileController

--- backend/controllers/searchProfileController.js
+++ backend/controllers/searchProfileController.js
@@ -16,23 +16,25 @@ const tiktokSearch = (username) => {
   return searchTiktok(username);
 };
 const searchProfilesController = async (req, res) => {
-  res.writeHead(200, "Access-Control-Allow-Origin: *", {
+  res.writeHead(200, {
+    "Access-Control-Allow-Origin": "*",
     "Access-Control-Expose-Headers":
       "*, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Access-Control-Allow-Methods, Access-Control-Max-Age",
     "Content-Type": "text/event-stream",
     Connection: "keep-alive",
     "Cache-Control": "no-cache",
   });
-  await Promise.all([
-    twitterSearch(req.params.username),
-    redditSearch(req.params.username),
-    twitchSearch(req.params.username),
-  ]).then((results) =>
-    results.map((result) => {
-      res.write("data: " + JSON.stringify(result) + "\n\n");
-    })
-  );
-  await tiktokSearch(req.params.username).then((result) => {
+
+  twitterSearch(req.params.username).then((result) => {
+    res.write("data: " + JSON.stringify(result) + "\n\n");
+  });
+  redditSearch(req.params.username).then((result) => {
+    res.write("data: " + JSON.stringify(result) + "\n\n");
+  });
+  twitchSearch(req.params.username).then((result) => {
+    res.write("data: " + JSON.stringify(result) + "\n\n");
+  });
+  tiktokSearch(req.params.username).then((result) => {
     res.write("data: " + JSON.stringify(result) + "\n\n");
   });
 };
