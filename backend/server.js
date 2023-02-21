07b9c808c7301c3cec21321674c3e6b31bfa1b05
Maryam Khan <maryam.khan@pcc.edu>
2023-02-20 19:54:21 -0800

    Added cors configuration to force presence of Access-Control-Allow-Headers and Access-Control-Max-Age headers during requests

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,15 @@ const app = express();
 
 app.use(cors());
 app.options("*", cors());
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Max-Age", "86400"); // 24 hours
+  next();
+});
+
 app.use(express.json());
 
 app.use("/search", searchProfileRoutes);
