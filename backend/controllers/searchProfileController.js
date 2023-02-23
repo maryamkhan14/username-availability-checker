9bc114f2683e0d3da6af227124835ba43a32baeb
Maryam Khan <maryam.khan@pcc.edu>
2023-02-22 18:55:39 -0800

    Placing searches back into searchProfileController to work around write after end error

--- backend/controllers/searchProfileController.js
+++ backend/controllers/searchProfileController.js
@@ -1,6 +1,22 @@
-const { searchAllNetworks } = require("../services/searchAllNetworks");
+const { searchTwitter } = require("../services/searchTwitter");
+const { searchTiktok } = require("../services/searchTiktok");
+const { searchTwitch } = require("../services/searchTwitch");
+const { searchReddit } = require("../services/searchReddit");
+
+const twitterSearch = (username) => {
+  return searchTwitter(username);
+};
+const redditSearch = (username) => {
+  return searchReddit(username);
+};
+const twitchSearch = (username) => {
+  return searchTwitch(username);
+};
+const tiktokSearch = (username) => {
+  return searchTiktok(username);
+};
 const searchProfilesController = async (req, res) => {
-  await res.writeHead(200, {
+  res.writeHead(200, {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Expose-Headers":
       "*, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Access-Control-Allow-Methods, Access-Control-Max-Age",
@@ -9,6 +25,27 @@ const searchProfilesController = async (req, res) => {
     "Cache-Control": "no-cache",
   });
 
-  await searchAllNetworks(res, req.params.username);
+  await Promises.allSettled([
+    twitterSearch(req.params.username)
+      .then((result) => {
+        res.write("data: " + JSON.stringify(result) + "\n\n");
+      })
+      .then(resolve()),
+    redditSearch(req.params.username)
+      .then((result) => {
+        res.write("data: " + JSON.stringify(result) + "\n\n");
+      })
+      .then(resolve()),
+    twitchSearch(req.params.username)
+      .then((result) => {
+        res.write("data: " + JSON.stringify(result) + "\n\n");
+      })
+      .then(resolve()),
+    tiktokSearch(req.params.username)
+      .then((result) => {
+        res.write("data: " + JSON.stringify(result) + "\n\n");
+      })
+      .then(resolve()),
+  ]);
 };
 module.exports = { searchProfilesController };
