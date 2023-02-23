cc9463d0b0c8ad80e04227696a32860d21009e9e
Maryam Khan <maryam.khan@pcc.edu>
2023-02-22 18:13:49 -0800

    Refactored searchProfileController's searches into searchAllNetworks and placed res.end() statement in searchProfileController

--- backend/controllers/searchProfileController.js
+++ backend/controllers/searchProfileController.js
@@ -1,22 +1,6 @@
-const { searchTwitter } = require("../services/searchTwitter");
-const { searchTiktok } = require("../services/searchTiktok");
-const { searchTwitch } = require("../services/searchTwitch");
-const { searchReddit } = require("../services/searchReddit");
-
-const twitterSearch = (username) => {
-  return searchTwitter(username);
-};
-const redditSearch = (username) => {
-  return searchReddit(username);
-};
-const twitchSearch = (username) => {
-  return searchTwitch(username);
-};
-const tiktokSearch = (username) => {
-  return searchTiktok(username);
-};
+const { searchAllNetworks } = "../services/searchAllNetworks";
 const searchProfilesController = async (req, res) => {
-  res.writeHead(200, {
+  await res.writeHead(200, {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Expose-Headers":
       "*, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Access-Control-Allow-Methods, Access-Control-Max-Age",
@@ -25,17 +9,7 @@ const searchProfilesController = async (req, res) => {
     "Cache-Control": "no-cache",
   });
 
-  twitterSearch(req.params.username).then((result) => {
-    res.write("data: " + JSON.stringify(result) + "\n\n");
-  });
-  redditSearch(req.params.username).then((result) => {
-    res.write("data: " + JSON.stringify(result) + "\n\n");
-  });
-  twitchSearch(req.params.username).then((result) => {
-    res.write("data: " + JSON.stringify(result) + "\n\n");
-  });
-  tiktokSearch(req.params.username).then((result) => {
-    res.write("data: " + JSON.stringify(result) + "\n\n");
-  });
+  await searchAllNetworks(res, req.params.username);
+  await res.end();
 };
 module.exports = { searchProfilesController };
--- backend/services/searchAllNetworks.js
+++ backend/services/searchAllNetworks.js
@@ -2,16 +2,18 @@ const { searchTwitter } = require("./searchTwitter");
 const { searchTwitch } = require("./searchTwitch");
 const { searchReddit } = require("./searchReddit");
 const { searchTiktok } = require("./searchTiktok");
-const searchAllNetworks = async (username) => {
-  const twitterData = searchTwitter(username);
-  const twitchData = searchTwitch(username);
-  const redditData = searchReddit(username);
-  const tikTokData = searchTiktok(username);
-  return {
-    twitterData: twitterData,
-    twitchData: twitchData,
-    redditData: redditData,
-    tikTokData: tikTokData,
-  };
+const searchAllNetworks = async (res, username) => {
+  searchTwitter(username).then((result) => {
+    res.write("data: " + JSON.stringify(result) + "\n\n");
+  });
+  searchReddit(username).then((result) => {
+    res.write("data: " + JSON.stringify(result) + "\n\n");
+  });
+  searchTwitch(username).then((result) => {
+    res.write("data: " + JSON.stringify(result) + "\n\n");
+  });
+  searchTiktok(username).then((result) => {
+    res.write("data: " + JSON.stringify(result) + "\n\n");
+  });
 };
 module.exports = { searchAllNetworks };
