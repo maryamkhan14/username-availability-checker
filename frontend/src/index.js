9539fee61dd4e26749ef42c1db7f85a17547a7ae
Maryam Khan <maryam.khan@pcc.edu>
2023-02-25 10:51:04 -0800

    Edited index.js  styling

---
+++ frontend/src/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import themeGlobal from "./styling/themes";
+import { ThemeProvider, CssBaseline, Box } from "@mui/material";
+import { AvailabilityContextProvider } from "./context/AvailabilityContext";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <AvailabilityContextProvider>
+    <ThemeProvider theme={themeGlobal}>
+      <CssBaseline />
+      <Box
+        height="100vh"
+        display="flex"
+        flexDirection="column"
+        justifyContent="space-around"
+      >
+        <App />
+      </Box>
+    </ThemeProvider>
+  </AvailabilityContextProvider>
+);
